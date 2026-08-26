65f9d1055f67096f319e29630541172c44c44109
heejun2822 <godfam44@gmail.com>
2026-08-26 20:25:18 +0900

    GenshinImpactMovementSystem / Rotating the Player

--- GenshinImpactMovementSystem/Assets/Scripts/Characters/Player/Player.cs
+++ GenshinImpactMovementSystem/Assets/Scripts/Characters/Player/Player.cs
@@ -6,16 +6,18 @@ namespace GenshinImpactMovementSystem
     public class Player : MonoBehaviour
     {
         public Rigidbody Rigidbody { get; private set; }
-
         public PlayerInput Input { get; private set; }
+
+        public Transform MainCameraTransform { get; private set; }
 
         private PlayerMovementStateMachine movementStateMachine;
 
         private void Awake()
         {
             Rigidbody = GetComponent<Rigidbody>();
-
             Input = GetComponent<PlayerInput>();
+
+            MainCameraTransform = Camera.main.transform;
 
             movementStateMachine = new PlayerMovementStateMachine(this);
         }
