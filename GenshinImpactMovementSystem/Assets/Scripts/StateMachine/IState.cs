b594709f11e5629826bccc316b57dcc8a105332f
heejun2822 <godfam44@gmail.com>
2026-08-26 15:32:47 +0900

    GenshinImpactMovementSystem / States and State Machines

---
+++ GenshinImpactMovementSystem/Assets/Scripts/StateMachine/IState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GenshinImpactMovementSystem
+{
+    public interface IState
+    {
+        public void Enter();
+        public void Exit();
+        public void HandleInput();
+        public void Update();
+        public void PhysicsUpdate();
+    }
+}
